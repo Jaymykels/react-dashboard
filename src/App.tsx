b34af86e5ca10a6aaa0fe1085da7a9cc45b4c9c9
Jaymykels <jaymykels69@gmail.com>
2021-09-03 12:41:40 +0100

    integrate apollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import {
+  useQuery,
+  gql
+} from "@apollo/client";
+
+const ALL_ACCOUNTS = gql`
+  query GetAllAccounts {
+    allAccounts {
+      id
+      first_name
+    }
+  }
+`;
+
+interface GetAllAccountsData {
+  allAccounts: Account[]
+}
+interface Account {
+  id: string;
+  first_name: string
+}
 
 function App() {
+  const { loading, error, data } = useQuery<GetAllAccountsData>(ALL_ACCOUNTS);
+  const [accounts, setAccounts] = useState([] as Account[])
+
+  useEffect(() => {
+    if(data)
+      setAccounts(data.allAccounts)
+  }, [data])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { accounts.length > 0 ? accounts[0].first_name : '' }
     </div>
   );
 }
